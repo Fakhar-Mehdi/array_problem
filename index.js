be8900d5ba37bfc392e3a28cae8a69f26ecc5c85
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-27 19:45:23 +0500

    solution implemented

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+const getObject = (arr, obj) => obj ? {
+    id: obj.id,
+    next: obj.next,
+    value: obj.value || getObject(arr, arr.find(e => e.id === obj.next)),
+} : null;
+
+const arrayProblemSolver = arr => arr.map(obj => getObject(arr, obj));
+
 const array = [
-    {
-        id: 'one',
-        next: 'two',
-        value: null
-    },
-    {
-        id: 'two',
-        next: 'three',
-        value: null
-    },
-    {
-        id: 'three',
-        next: null,
-        value: null
-    },
+    { id: 'one', next: 'two', value: null },
+    { id: 'two', next: 'three', value: null },
+    { id: 'three', next: null, value: null },
 ];
+
+console.log(arrayProblemSolver(array));
